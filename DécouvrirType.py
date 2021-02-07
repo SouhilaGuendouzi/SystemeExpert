8740a491d9ab73ae26884c4e7186986826604b93
SouhilaGuendouzi <61289836+SouhilaGuendouzi@users.noreply.github.com>
2021-02-07 22:25:37 +0100

    Structure fixed

--- DécouvrirType.py
+++ DécouvrirType.py
@@ -1,66 +1,94 @@
-class BaseConnaisance:
-    def __init__(self, Règles, BaseFait):
-        self.Règles=Règles
-        self.BaseFait=BaseFait
+from typing import Type
+
+
 class Regle:
+    Listprémisses: list
+    Listconclusions: list
+
     def __init__(self, Listprémisses, Listconslusions):
-        self.Listprémisses=Listprémisses
-        self.Listconslusions=Listconslusions
+        self.Listprémisses = Listprémisses
+        self.Listconslusions = Listconslusions
+
+
+class BaseConnaisance:
+    Règles: list[Regle]
+    BaseFait: Type[list]
+
+    def __init__(self, Règles, BaseFait):
+        self.Règles = Règles
+        self.BaseFait = BaseFait
+
+
 class DécouvrirType:
-    def __init__(self,file1,file2):
-        self.file1=file1;
+    def __init__(self, file1, file2):
+        self.file1 = file1;
         self.file2 = file2;
 
     def Découvrir(self):
-        listPrémisse=[]  #cette liste a pour but d'insérer les prémisses
-        listConclusion=[] #cette liste a pour but d'insérer les conclusions
-        listBaseFait=[] #cette liste a pour but d'insérer les fait lus par le fichier 2
-        listRègles=[] #cette liste a pour but d'insérer les règles
+        listPrémisse = []    # cette liste a pour but d'insérer les prémisses
+        listConclusion = []  # cette liste a pour but d'insérer les conclusions
+        listBaseFait = []    # cette liste a pour but d'insérer les fait lus par le fichier 2
+        listRègles = []      # cette liste a pour but d'insérer les règles
         f = open(self.file1, "r")
-        stop=False
+        stop = False
         for x in f:
+            listConclusion.clear()
+            listPrémisse.clear()
             un = x.split(":")
-            deux =un[1].split(",")
+            deux = un[1].split(",")
             trois = deux[0].split("SI")
-            cET=trois[1].split(" ").count("et") #compter le nombre de ET
-            cOU=trois[1].split(" ").count("ou") #compter le nombre de OU
-            quatre =trois[1].split("et")
-            cinq=trois[1].split("ou")
-            six =deux[1].split("ALORS")[1].split(" ");
-            for j in six[1]:
-                if (j not in listConclusion and j!="\n"):
-                    listConclusion.append(j)
-            if (len(quatre)==cET+1 and cOU==0):
-               for i in quatre:
-                  if (i not in listPrémisse):
-                    listPrémisse.append(i)
-               R=Regle(listPrémisse,listConclusion)
-               listConclusion.clear()
-               listPrémisse.clear()
-               listRègles.append(R)
-            elif (len(cinq)==cOU+1 and cET==0):
+            cET = trois[1].split(" ").count("et")  # compter le nombre de ET
+            cOU = trois[1].split(" ").count("ou")  # compter le nombre de OU
+            quatre = trois[1].split("et")
+            cinq = trois[1].split("ou")
+            six = deux[1].split("ALORS")[1]
+            sept = six.split("et")
+            for j in sept:  # hna khdamt b j[1] bah matadkhoulich "\n" maahoum
+                if (j[1] not in listConclusion):
+                    listConclusion.append(j[1])
+
+            if (len(quatre) == cET + 1 and cOU == 0):
+                for i in quatre:
+                    if (i not in listPrémisse):
+                        listPrémisse.append(i)
+                LISTp = []
+                LISTc = []
+                for t in listPrémisse:
+                    LISTp.append(t)
+                for k in listConclusion:
+                    LISTc.append(k)
+                R = Regle(LISTp, LISTc)
+                listRègles.append(R)
+            elif (len(cinq) == cOU + 1 and cET == 0):
                 for i in cinq:
                     if (i not in listPrémisse):
-                         listPrémisse.append(i)
-                R=Regle(listPrémisse,listConclusion)
-                listConclusion.clear()
-                listPrémisse.clear();
+                        listPrémisse.append(i)
+                LISTp=[]
+                LISTc=[]
+                for t in listPrémisse:
+                    LISTp.append(t)
+                for k in listConclusion:
+                    LISTc.append(k)
+                R = Regle(LISTp, LISTc)
                 listRègles.append(R)
-            else :
-                stop=True
-        if (stop==False):
-              f2= open(self.file2, "r")
-              for j in f2:
-                  n=j.split("\n")[0]
-                  for z in n :
-                      if z not in listBaseFait :
-                       listBaseFait.append(z)
-              BaseConnaisanceA=BaseConnaisance(listRègles,listBaseFait)
-              print(len(BaseConnaisanceA.Règles))
-              return("RAHA mliha n9adou nabdou l khadma taana ")
+            else:
+                stop = True
+        if (stop == False):  # pas de conflit on passe à l'étape suivante ; extraction de la base de fait
+            f2 = open(self.file2, "r")
+            for j in f2:
+                n = j.split("\n")[0]
+                for z in n:
+                    if z not in listBaseFait:
+                        listBaseFait.append(z)
+            BaseConnaisanceA = BaseConnaisance(listRègles, listBaseFait)
+
+            return BaseConnaisanceA
 
         else:
-            return ("hna wi ykono les règles fihoum ET w OU maa Ba3de")
-
-c = DécouvrirType("fichier.txt","baseFait.txt")
-print(c.Découvrir())
+            return None
+c = DécouvrirType("fichier.txt", "baseFait.txt")
+BaseConnaisanceB = c.Découvrir()
+if BaseConnaisanceB!=None:
+      print(BaseConnaisanceB.Règles[0].Listconslusions) #j'étais entrain de faire le test
+else :
+    print("on a pas traiter ce cas")
